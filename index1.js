b894fe3f4fe17bc065d5ba8843b388b1c67f5797
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-24 10:45:58 +0600

    array && object Destructuring

---
+++ index1.js
@@ -0,0 +1,25 @@
+// Array Destructuring
+const myFriend = ["Hamim", "Asif", "Ontu"];
+const [bFriend, nFriend, gFriend] = myFriend;
+
+// console.log(bFriend);
+// console.log(nFriend);
+// console.log(gFriend);
+
+// object Destructuring
+
+const myAddress = {
+  house: 10,
+  block: "c",
+  road: "Sholok Road",
+  area: "Jugihati",
+  location: "Barishal",
+};
+
+const { house, block, road, area, location } = myAddress;
+
+// console.log(house);
+// console.log(block);
+// console.log(road);
+// console.log(area);
+// console.log(location);
